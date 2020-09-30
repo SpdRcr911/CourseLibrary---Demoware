10ebbc68d8d50d18b58c53c8d2eb245ad831455d
Rafael Delgado <rafael@delgado.com>
2020-09-29 19:14:13 -0700

    Add AuthorsController First pass

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,27 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository courseLibraryRepository;
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            this.courseLibraryRepository = courseLibraryRepository ?? 
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        public IActionResult GetAuthors()
+        {
+            return Ok(courseLibraryRepository.GetAuthors());
+        }
+    }
+}
